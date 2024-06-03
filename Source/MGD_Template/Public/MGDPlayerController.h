5bd58c56be9cd43b271c021fc3d90712cad369b7
SnazzSnek <atdam4987@gmail.com>
2024-06-03 15:19:32 +1000

    I think it worked?

---
+++ Source/MGD_Template/Public/MGDPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MGDPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MGD_TEMPLATE_API AMGDPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};

4458121fb9fa1f2990092cb8806d7fbb078ac069
SnazzSnek <atdam4987@gmail.com>
2024-06-03 15:28:17 +1000

    is blue now

--- Source/MGD_Template/Private/MGDPlayerController.cpp
+++ Source/MGD_Template/Private/MGDPlayerController.cpp
@@ -3,3 +3,20 @@
 
 #include "MGDPlayerController.h"
 
+void AMGDPlayerController::OnRep_Pawn()
+{
+	Super::OnRep_Pawn();
+
+	PureOnRepPosses(GetPawn());
+}
+
+void AMGDPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	PureOnRepPosses(InPawn);
+
+
+
+	
+}
--- Source/MGD_Template/Public/MGDPlayerController.h
+++ Source/MGD_Template/Public/MGDPlayerController.h
@@ -13,5 +13,15 @@ UCLASS()
 class MGD_TEMPLATE_API AMGDPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	virtual void OnRep_Pawn() override;
+
+	virtual void OnPossess(APawn* InPawn) override;
+
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category="Replication")
+	void PureOnRepPosses(APawn* PossesedPawn);
 	
 };
